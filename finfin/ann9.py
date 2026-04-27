9664b20267f1d8f2b9cdcf2437ca2662c83c89f9
varsharao2005 <varsharao2005@gmail.com>
2026-04-27 16:14:07 +0530

    changed 9

--- finfin/ann9.py
+++ finfin/ann9.py
@@ -1,12 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-x=np.linspace(0,10,100)
-y=np.sin(x)+np.random.rand(100)*0.2
-f=np.convolve(y,[1,0,-1],mode='same')
-
-plt.plot(y,label="Input Signal")
-plt.plot(f,label="Feature Map")
-plt.title("CNN Feature Extraction Analysis")
-plt.legend()
+X = np.random.rand(100, 28, 28, 1)
+y = np.random.randint(0, 2, 100)
+# Build CNN Model
+model = Sequential()
+model.add(Conv2D(8, (3,3), activation='relu', input_shape=(28,28,1)))
+model.add(MaxPooling2D((2,2)))
+model.add(Flatten())
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+# Train model
+history = model.fit(X, y, epochs=10, verbose=0)
+# Plot training accuracy
+plt.plot(history.history['accuracy'])
+plt.title("CNN Training Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
 plt.show()
